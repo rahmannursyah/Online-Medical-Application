ba8263cf1b3c75add21bc00d16f0e1c81df19d4c
Rahmannur Rizki Syahputra <rahmannur@getgo.sg>
2023-11-14 19:51:38 +0700

    inital: added xcode workspace with podfile

---
+++ Online-Medical-Application/Online-Medical-Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Online-Medical-Application
+//
+//  Created by Rahmannur Rizki Syahputra on 14/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
